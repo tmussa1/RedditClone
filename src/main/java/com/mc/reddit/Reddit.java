a3549899fdb2430b166f2c4b6866bbcde3819672
Tofik Mussa <tofahub@gmail.com>
2018-06-26 17:06:14 -0400

    COmmit

--- src/main/java/com/mc/reddit/Reddit.java
+++ src/main/java/com/mc/reddit/Reddit.java
@@ -16,6 +16,9 @@ public class Reddit {
 
     private String title;
     private String name;
+    private String URLtitle;
+    private String imageURL;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd 'T' HH:mm")
     private LocalDateTime date;
 
@@ -26,9 +29,24 @@ public class Reddit {
     public void setDate(LocalDateTime date) {
         this.date = date;
     }
+    public String getURLtitle() {
+        return URLtitle;
+    }
+
+    public void setURLtitle(String URLtitle) {
+        this.URLtitle = URLtitle;
+    }
 
     public String getTitle() {
         return title;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
     }
 
     public void setTitle(String title) {
